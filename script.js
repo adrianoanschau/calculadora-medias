ab5f55eda51c125294789c302bbf42ed669e51e9
Adriano Anschau <adrianoanschau@gmail.com>
2026-03-30 21:14:26 -0300

    Resolução

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
     /**
      * ESPAÇO PARA O DESAFIO
      * Instruções: 
@@ -14,6 +13,15 @@
         
         // Exemplo de como capturar:
         // let n1 = parseFloat(document.getElementById('n1').value);
+        let n1 = parseFloat(document.getElementById('n1').value);
+        let n2 = parseFloat(document.getElementById('n2').value);
+        let n3 = parseFloat(document.getElementById('n3').value);
+        let n4 = parseFloat(document.getElementById('n4').value);
+
+        const total = n1 + n2 + n3 + n4;
+        const media = total / 4;
+
+        outputDiv.innerText = "Média: " + media
 
         // --- SEU CÓDIGO TERMINA AQUI ---
     }
